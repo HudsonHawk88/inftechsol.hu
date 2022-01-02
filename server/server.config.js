98246f42e0219c3f3245910f03ccb46e69782a7f
HudsonHawk88 <toth.gergo88@hotmail.com>
2022-01-02 23:57:57 +0100

    fix(ElerhetosegekSerice): fix JSON error

--- server/server.config.js
+++ server/server.config.js
@@ -3,10 +3,14 @@ module.exports = {
       name   : "backend",
       script : "./index.js",
       env_production: {
-        NODE_ENV: "production"
+        NODE_ENV: "production",
+        HOST: '127.0.0.1',
+        PORT: 8081
       },
       env_development: {
-        NODE_ENV: "development"
+        NODE_ENV: "development",
+        HOST: '127.0.0.1',
+        PORT: 8081
       },
       exec_mode: "cluster",
       autorestart: true,
