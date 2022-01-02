6c2253aeb924d82becde96efcc05b733a3ed00f9
HudsonHawk88 <toth.gergo88@hotmail.com>
2022-01-03 00:05:27 +0100

    fix(ElerhetosegekSerice): fix JSON error

--- server/server.config.js
+++ server/server.config.js
@@ -2,15 +2,16 @@ module.exports = {
     apps : [{
       name   : "backend",
       script : "./index.js",
-      env_production: {
+      env: {
         NODE_ENV: "production",
         HOST: '127.0.0.1',
-        PORT: 8081
-      },
-      env_development: {
-        NODE_ENV: "development",
-        HOST: '127.0.0.1',
-        PORT: 8081
+        PORT: 8081,
+        KEY_FILE: '/etc/letsencrypt/live/inftechsol.hu/privkey.pem',
+        CERT_FILE:'/etc/letsencrypt/live/inftechsol.hu/cert.pem',
+        FULL_FILE:'/etc/letsencrypt/live/inftechsol.hu/fullchain.pem',
+        JWT_EXPIRE:'30s',
+        JWT_SECRET:'{8883TGSZI-HLS101-ZEG-0318-20102013AG}',
+        JWT_REFRESH_SECRET:'{20102013AG-0318-ZEG-HLS101-8883TGSZI}'
       },
       exec_mode: "cluster",
       autorestart: true,
